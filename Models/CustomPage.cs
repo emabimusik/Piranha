9912cebfb30e1de0f8ddfdd2f53bca6fa25b6e21
emabi20 <abiyoabiyo59@hotmail.com>
2020-02-03 13:09:34 +0100

    first commit

---
+++ Models/CustomPage.cs
@@ -0,0 +1,37 @@
+﻿using Northwind.Models.Regions;
+using Piranha.AttributeBuilder;
+using Piranha.Extend;
+using Piranha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Models
+{
+
+    [PageType(Title = "Custom page")]
+    [PageTypeRoute(Title = "Custom", Route = "/custom")]
+    public class CustomPage:Page<CustomPage>
+    {
+        /// <summary>
+        /// Gets/sets the page hero.
+        /// </summary>
+        [Region(Display = RegionDisplayMode.Setting)]
+        public Hero Hero { get; set; }
+
+        /// <summary>
+        /// Gets/sets the available teasers.
+        /// </summary>
+        [Region(ListTitle = "Title")]
+        public IList<Teaser> Teasers { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public CustomPage()
+        {
+            Teasers = new List<Teaser>();
+        }
+    }
+}
